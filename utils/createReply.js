01b29983f1c4cb941d889d791a48368d56d0dab8
KarlisLauris <karlislauris@gmail.com>
2023-04-06 00:26:47 +0300

    Deleted createReply.js

--- utils/createReply.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    createReply: async function (interaction, content, ephemeral) {
-        if (ephemeral == true) {
-            await interaction.reply({ content: content, ephemeral: true });
-        } else {
-            await interaction.reply(content);
-        }
-    }
-}
